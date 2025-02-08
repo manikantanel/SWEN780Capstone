c6f50ec84bd186095e906bf6f20657bd858d78f8
manikantane <manikantanel@gmail.com>
2025-02-07 22:49:02 -0500

    add train model

---
+++ train_model.py
@@ -0,0 +1,40 @@
+import numpy as np
+import os
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+DATA_PATH = os.path.join('data_collection')
+actions = np.array(["Hello", "Thank you", "Have a good day"])
+sequence_length = 30
+
+sequences, labels = [], []
+label_map = {label: num for num, label in enumerate(actions)}
+
+for action in actions:
+    for sequence in os.listdir(os.path.join(DATA_PATH, action)):
+        window = []
+        for frame_num in range(sequence_length):
+            res = np.load(os.path.join(DATA_PATH, action, sequence, f"{frame_num}.npy"))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+y = to_categorical(labels)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+
+model = Sequential([
+    LSTM(64, return_sequences=True, activation='relu', input_shape=(sequence_length, X.shape[2])),
+    LSTM(128, return_sequences=False, activation='relu'),
+    Dropout(0.2),
+    Dense(64, activation='relu'),
+    Dense(actions.shape[0], activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+history = model.fit(X_train, y_train, epochs=500, validation_data=(X_test, y_test), batch_size=32)
+
+model.save('action_1.h5')
