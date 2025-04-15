327f764317fd100faf6432f9116e8214334cbf0c
manikantane <manikantanel@gmail.com>
2025-04-15 18:45:45 -0400

    deleted

--- convert_h5.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load the existing model without compiling
-model = load_model('action_1.h5', compile=False)
-
-# Save the model again in a format compatible with TensorFlow 2.18
-model.save('action_1_converted.h5', save_format='h5')
-
-print("Model converted and saved as action_1_converted.h5")
